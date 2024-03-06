89d077eaeb5851602fcbf0d2eda6df126373e56d
Arshavin023 <uchejudennodim@gmail.com>
2024-03-06 14:34:29 +0100

    django beginner ended

--- src/users/views.py
+++ src/users/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
+from django.views import View 
 
 # Create your views here.
 def login_view(request):
@@ -13,13 +14,37 @@ def login_view(request):
             user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
-                messages.success(
-                    request, f"You are now logged in as {username}"
-                    )
+                messages.success(request, f"You are now logged in as {username}")
                 return redirect('home')
             else:
-                messages.error()
+                messages.error(request, f'An error occured trying to login')
+        else:
+                messages.error(request, f'An error occured trying to login')
+                
     elif request.method == 'GET':
         login_form = AuthenticationForm()
     return render(request, 'views/login.html', {'login_form': login_form})
+
+
+class RegisterView(View):
+
+    def get(self, request):
+        register_form = UserCreationForm()
+        return render(request, 'views/register.html', {'register_form':register_form})
+
+    def post(self, request):
+        register_form = UserCreationForm(data=request.POST)
+        if register_form.is_valid():
+            user =  register_form.save()
+            user.refresh_from_db()
+            # password = register_form.cleaned_data.get('password')
+            # user = authenticate(username=user.username, password=password)
+            login(request, user)
+            messages.success(
+                request, f'User {user.username} register successfully. ')
+            return redirect('home')
+        else:
+            messages.error(
+                request, f'An error occured while trying to register. ')
+            return render(request, 'views/register.html', {'register_form':register_form})
 
